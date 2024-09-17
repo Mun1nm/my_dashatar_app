d2619847e56aa8f37a832aa443b67ca0bb7ba0e6
Mun1nm <joaoaveraldo@yahoo.com.br>
2024-09-17 17:40:26 -0300

    Stateful Widgets, Random and Audio

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,67 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:math';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int count = 0;
+
+  List<Color> colours = [
+    Colors.white,
+    Colors.black,
+    Colors.blue,
+    Colors.red,
+    Colors.green,
+    Colors.yellow
+  ];
+  int appBar = 0;
+  int scaffold = 0;
+
+  Random random = new Random();
+  List sounds = ["Kick.wav", "Ride.wav", "Snare.wav", "Tom.wav"];
+
+  int soundPosition = 0;
+
+  final player = AudioPlayer();
+
+  @override
   Widget build(BuildContext context) {
-    List<Text> a = [Text("Dash1"), Text("Dash2")];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: colours[appBar],
           centerTitle: true,
-          title: Text("My Dashtar App"),
+          title: IconButton(
+            icon: Icon(Icons.refresh, color: Colors.blueAccent),
+            onPressed: () {
+              soundPosition = random.nextInt(4);
+            },
+          ),
         ),
-        body: Stack(
-          children: [
-            Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.yellow,
-              //child: Image.asset('images/dash.png'),
-            ),
-            Positioned(
-              left: 10.0,
-              top: 20.0,
-              child: Container(
-                height: 50.0,
-                width: 50.0,
-                color: Colors.blue,
-                //child: Image.asset('images/dash.png'),
-              ),
-            ),
-          ],
+        backgroundColor: colours[scaffold],
+        body: MaterialButton(
+          child: Image.asset('images/dash.png'),
+          onPressed: () {
+            setState(() {
+              player.play(AssetSource(sounds[soundPosition]));
+              appBar = random.nextInt(6);
+              scaffold = random.nextInt(6);
+            });
+          },
         ),
       ),
     );
@@ -45,12 +69,7 @@ class MyApp extends StatelessWidget {
 }
 
 /*
-Container(
-height: 100.0,
-width: 100.0,
-color: Colors.red,
-child: Image.asset('images/dash.png'),
-),
+
 Container(
 height: 100.0,
 width: 100.0,
